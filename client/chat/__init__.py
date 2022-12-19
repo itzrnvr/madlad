f9a3e066f338d02c368be85de8e135da8470e89f
Anatame <anata.me97@gmail.com>
2022-12-20 04:09:13 +0530

    added on message

--- client/chat/__init__.py
+++ client/chat/__init__.py
@@ -7,17 +7,27 @@ import sys
 
 class ChatManager:
     def __init__(self, clientID):
+        self.onMes = None
         self.ws = create_connection("wss://concord-28ll.onrender.com/api/v1/chats/all/ws/" + clientID)
         readThread = StoppableThread(target=self.read)
         readThread.start()
         # writeThread = StoppableThread(target=self.write)
 
     def read(self):
-        while True:
+        alive = True
+        while alive:
             try:
-                print("Server ", self.ws.recv())
+                mes = self.ws.recv()
+                jsonMes = json.load(mes)
+                if self.onMes is not None:
+                    self.onMes(jsonMes["username"], jsonMes["message"])
+                print("Server ", mes)
             except:
+                alive = False
                 print("Exiting read")
+
+    def onMessage(self, fn):
+        self.onMes = fn
 
     def sendMessage(self, username, message):
         mes = {"username": username, "message": message}
